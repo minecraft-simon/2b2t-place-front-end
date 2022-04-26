80867f5dc72d51d7a90fecd74452e3b6bc87aafd
minecraft-simon <minecraft_simon@gmx.net>
2022-04-26 15:38:40 +0200

    new logo

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar app dark flat class="place-app-bar">
     <div class="d-flex align-center">
-      <v-img class="" contain src="@/assets/2b2t.png" width="40"/>
+      <v-img class="" contain src="@/assets/logo.png" width="40"/>
       <v-img class="ml-2" contain height="36" max-width="160" src="@/assets/place.png"/>
     </div>
 
